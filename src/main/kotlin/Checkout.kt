cdaf1e5400cb14c88fb1486c4aaf0bcc54437dfa
Arfaz <aferoz@hotmail.fr>
2020-07-25 17:29:40 +0800

    First commit

---
+++ src/main/kotlin/Checkout.kt
@@ -0,0 +1,14 @@
+package com.checkout
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+
+@EnableAutoConfiguration
+@ComponentScan
+class Checkout
+
+fun main(args: Array<String>) {
+    runApplication<Checkout>(*args)
+}
